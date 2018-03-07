1690587799c25abb8e47dfca9eefce0e24d24009
yangshiwei <yangshiwei@syhvip.com>
2018-03-07 16:15:37 +0800

    修改android7以及以上版本兼容性问题bug

---
+++ src/android/ImageProvider.java
@@ -0,0 +1,8 @@
+package cordovawechattimelineshare;
+
+import android.support.v4.content.FileProvider;
+
+public class ImageProvider extends FileProvider {
+   
+
+}
--- src/android/ShareUtils.java
+++ src/android/ShareUtils.java
@@ -6,7 +6,8 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.net.Uri;
 import android.widget.Toast;
-
+import android.support.v4.content.FileProvider;
+import android.provider.MediaStore;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -52,20 +53,35 @@ public class ShareUtils {
      * @param Kdescription 9图上边输入框中的文案
      * @param paths        本地图片的路径
      */
-    public static void share9PicsToWXCircle(Context context, String Kdescription, List<String> paths) {
+    public static void share9PicsToWXCircle(Context context, String Kdescription, List<String> paths){
         if (!isInstallWeChart(context)) {
             Toast.makeText(context,"您没有安装微信",Toast.LENGTH_SHORT).show();
             return;
         }
         Intent intent = new Intent();
         intent.setComponent(new ComponentName("com.tencent.mm", "com.tencent.mm.ui.tools.ShareToTimeLineUI"));
-        intent.setAction("android.intent.action.SEND_MULTIPLE");
-
         ArrayList<Uri> imageList = new ArrayList<Uri>();
         for (String picPath : paths) {
             File f = new File(picPath);
             if (f.exists()) {
-                imageList.add(Uri.fromFile(f));
+              try{
+                  if(paths.size()==1){
+                      //单张图片谁用fileprovide转换
+                      Uri photoUri = FileProvider.getUriForFile(
+                              context,
+                              context.getPackageName() + ".imageProvider",
+                              f);
+                      imageList.add(photoUri);
+                  }else{
+                      //通过cordova fileTransfer插件下载的图片不能在相册中看到,并且是file(android7以及以上版本已经禁止使用此头传递)开头的,可以通过MediaStore的api将图片插入到相册中
+                      //并返回一个content://开头的path,这样android7以及以上版本可以传递
+                      String imgpath=MediaStore.Images.Media.insertImage(context.getContentResolver(), f.getAbsolutePath(), f.getName(), null);
+                      imageList.add(Uri.parse(imgpath));
+                  }
+              }catch(Exception e){
+                Toast.makeText(context,"网络错误,请稍后再试",Toast.LENGTH_SHORT).show();
+                return;
+              }
             }
         }
         if(imageList.size() == 0){
@@ -73,11 +89,16 @@ public class ShareUtils {
             return;
         }
         intent.setType("image/*");
-        intent.putExtra(Intent.EXTRA_STREAM, imageList); //图片数据（支持本地图片的Uri形式）
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        if(paths.size()==1){
+            intent.setAction(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_STREAM,imageList.get(0));
+        }else{
+            intent.setAction(Intent.ACTION_SEND_MULTIPLE);
+            intent.putExtra(Intent.EXTRA_STREAM,imageList);
+        }
         intent.putExtra("Kdescription", Kdescription); //微信分享页面，图片上边的描述
-		//修改:兼容android7以及以上版本
-		context.startActivity(Intent.createChooser(intent,"微信分享"));
-        //context.startActivity(intent);
+        context.startActivity(intent);
     }
 
     /**不实用微信sdk检查是否安装微信
